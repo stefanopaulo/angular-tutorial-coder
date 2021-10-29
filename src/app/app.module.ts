8c964ad3e513863fc8cc020669fd9cf4cf96a7f3
Stefano Souza <stefano.desouza@vmetrix.com>
2021-10-29 16:28:55 -0300

    Update Product Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { ForDirective } from './directives/for.directive';
 import { RedDirective } from './directives/red.directive';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -44,7 +45,8 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductTableComponent
+    ProductTableComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
